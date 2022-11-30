bb727fdc2ab1b11b2fc7610ab2638b800f47442f
Ravindram333 <95265933+Ravindram333@users.noreply.github.com>
2022-12-01 00:11:05 +0530

    Update main.tf

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -107,8 +107,8 @@ resource "azurerm_windows_virtual_machine" "vm" {
   resource_group_name = azurerm_resource_group.rg01.name
   location            = azurerm_resource_group.rg01.location
   size                = "Standard_Ds1_v2"
-  admin_username      = data.azurerm_key_vault_secret.adminuser.name
-  admin_password      = data.azurerm_key_vault_secret.adminpassword.value
+  admin_username      = var.adminusername
+  admin_password      = var.adminpassword
   tags                = var.tags_name
   network_interface_ids = [
     azurerm_network_interface.nic.id,
